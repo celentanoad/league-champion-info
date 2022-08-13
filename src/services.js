b19a35443a4da408d69eda80f2c32f0c3195a197
Alanna Celentano <celentanoad91@gmail.com>
2022-08-13 14:23:45 -0500

    connect to API and set up search functionality

---
+++ src/services.js
@@ -0,0 +1,9 @@
+export const baseURL = 'https://league-of-legends-champions.p.rapidapi.com/champions/en-us?page=0&size=10';
+
+export const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '6e3711b29fmsh21e033f998be2cap1e7d9bjsneb454b90415c',
+    'X-RapidAPI-Host': 'league-of-legends-champions.p.rapidapi.com'
+  }
+};
