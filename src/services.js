152d00742707a91285c63329ec67feb441b51aab
Alanna Celentano <celentanoad91@gmail.com>
2022-08-13 14:42:55 -0500

    set up environment variables

--- src/services.js
+++ src/services.js
@@ -1,9 +1,8 @@
 export const baseURL = 'https://league-of-legends-champions.p.rapidapi.com/champions/en-us?page=0&size=10';
-
 export const options = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': '6e3711b29fmsh21e033f998be2cap1e7d9bjsneb454b90415c',
+    'X-RapidAPI-Key': `${process.env.REACT_APP_API_KEY}`,
     'X-RapidAPI-Host': 'league-of-legends-champions.p.rapidapi.com'
   }
 };
